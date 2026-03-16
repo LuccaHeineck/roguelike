c5f0ea039992fb9b19d13be04a5765d52ebf85d5
Daniel <daniel.corbellini28@gmail.com>
2026-03-15 23:33:49 -0300

    feat: implementa combo de ataques do player

--- Assets/Scripts/Player/States/PlayerAttackState.cs
+++ Assets/Scripts/Player/States/PlayerAttackState.cs
@@ -6,16 +6,26 @@ public class PlayerAttackState : IState
     private float timer;
     private const float attackDuration = 0.54f;
 
-    public PlayerAttackState(PlayerControl player) => this.player = player;
+    private int comboStep;
+    private bool comboQueued;
+    private const int maxComboStep = 2;
+
+    public PlayerAttackState(PlayerControl player, int step = 0)
+    {
+        this.player = player;
+        this.comboStep = step;
+    }
 
     public void Enter()
     {
+        comboQueued = false;
         timer = 0f;
         player.Rb.linearVelocity = Vector2.zero;
 
         player.Animator.SetFloat("lastInputX", player.LastMoveInput.x);
         player.Animator.SetFloat("lastInputY", player.LastMoveInput.y);
 
+        player.Animator.SetInteger("ComboStep", comboStep);
         player.Animator.SetTrigger("Attacking");
     }
 
@@ -23,8 +33,20 @@ public class PlayerAttackState : IState
     {
         timer += Time.deltaTime;
 
+        if (player.AttackPressed && !comboQueued)
+        {
+            comboQueued = true;
+            player.ConsumeAttack();
+        }
+
         if (timer >= attackDuration)
         {
+            if (comboQueued && comboStep < maxComboStep - 1)
+            {
+                player.StateMachine.ChangeState(new PlayerAttackState(player, comboStep + 1));
+                return;
+            }
+
             if (player.MoveInput != Vector2.zero)
                 player.StateMachine.ChangeState(new PlayerRunningState(player));
             else
