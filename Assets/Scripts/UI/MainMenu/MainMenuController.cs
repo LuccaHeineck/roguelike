642cbf2da6431ca4799b2161807d4e8b25171180
nokk409 <giovane.cardoso409@gmail.com>
2026-03-21 21:37:31 -0300

    feat: implementado prototipo de menu inicial

---
+++ Assets/Scripts/UI/MainMenu/MainMenuController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void BotaoNovoJogo()
+    {
+        SceneManager.LoadScene("testeOne");
+    }
+}
