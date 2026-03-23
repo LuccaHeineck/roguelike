005b3ebdb2ad5600613bc3209c2cdbc711b0ecda
Daniel <daniel.corbellini28@gmail.com>
2026-03-22 23:51:55 -0300

    feat: implementa transições de level

---
+++ Assets/Scripts/Transitions/LevelLoader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.capsLockKey.wasPressedThisFrame)
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int sceneIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
